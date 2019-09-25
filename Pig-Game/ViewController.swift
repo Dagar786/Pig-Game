a3891202f9f4ce7b98204f9f5932421e927be8c1
Dagar <sowdagar786@gmail.com>
2019-09-25 10:10:18 -0500

    Ready for backend connection

--- Pig-Game/ViewController.swift
+++ Pig-Game/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var diceImage: UIImageView!
+    @IBOutlet var p1PointsLable: UILabel!
+    @IBOutlet var p2PointsLable: UILabel!
+    @IBOutlet var currentPointLable: UILabel!
+    var playerTurn = 0
+    // var playerArray : [Player] = [Player, Player]
+    
+    let diceArray : [String] = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        diceImage.image = UIImage(named: "dice1")
+        
     }
 
-    @IBOutlet var diceImage: UIImageView!
+   
     
     @IBAction func dicePressed(_ sender: Any) {
         
-        
+        //let diceNumber = playerArray[playerTurn].roll()
+        //diceImage.image = UIImage(named: diceArray[diceNumber - 1])
+        //currentPointLable.text = "\(playerArray[playerTurn].getCurrentScore())"
+ 
+    }
+    
+    
+    @IBAction func endTurnPressed(_ sender: Any) {
+        //playerArray[playerTurn].endTurn()
+        if playerTurn == 0{
+            //p1PointsLable.text = layerArray[playerTurn].getTotalScore()
+        }
+        else if playerTurn == 1{
+            //p2PointsLable.text = layerArray[playerTurn].getTotalScore()
+        }
+        //currentPointLable.text = "\(playerArray[playerTurn].getCurrentScore())"
     }
     
     
